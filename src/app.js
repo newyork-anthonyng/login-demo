95f2536643a83a6f87d409b21538bab6f0c6a0c9
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-29 18:36:27 -0400

    add basic login page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,51 @@
 import React from "react";
 import { render } from "react-dom";
+import { Button } from "reakit/Button";
 import "./styles.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="bg-background1 h-full pt-8">
+      <div className="flex justify-center">
+        <div className="bg-white w-9/12 max-w-lg flex justify-center shadow-md py-10 px-10">
+          <form className="w-full">
+            <div className="mb-4 w-full">
+              <label className="block mb-1" for="email-input">
+                Email
+              </label>
+              <input
+                className="border border-gray-500 w-full"
+                type="text"
+                id="email-input"
+                value=""
+              />
+            </div>
+
+            <div className="mb-8 w-full">
+              <label className="block mb-1" for="password-input">
+                Password
+              </label>
+              <input
+                className="border border-gray-500 w-full"
+                type="password"
+                id="password-input"
+                value=""
+              />
+            </div>
+
+            <div className="flex justify-center w-full">
+              <Button
+                type="submit"
+                className="bg-button text-white uppercase px-6 py-2 rounded-sm"
+              >
+                Login
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 render(<App />, document.querySelector(".js-root"));
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        background1: "#E4F4D7",
+        button: "#00B3DB",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
