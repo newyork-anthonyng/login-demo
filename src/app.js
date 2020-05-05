eecbde5219fd2690a8879e2b5c0eccca7bcbb478
Anthony Ng <newyork.anthonyng@gmail.com>
2020-05-05 18:00:30 -0400

    add react-router

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import { render } from "react-dom";
 import LoginPage from "./LoginPage/index";
 
@@ -6,9 +13,27 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="bg-background1 h-full pt-8">
-      <LoginPage />
-    </div>
+    <Router>
+      <div className="bg-background1 h-full pt-8">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+
+          <Route>
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
