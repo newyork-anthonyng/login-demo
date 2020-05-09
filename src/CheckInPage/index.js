f4cb365597a2f303fb65abb915b426ca51b0e11a
Anthony Ng <newyork.anthonyng@gmail.com>
2020-05-09 13:27:15 -0400

    add todo inputs

--- src/CheckInPage/index.js
+++ src/CheckInPage/index.js
@@ -1,52 +1,73 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
 import todosMachine from "./todosMachine";
-import { retrieve, persist } from "./storage";
 import Todo from "./Todo";
 
-const persistedTodosMachine = todosMachine.withConfig(
-  {
-    actions: {
-      persist: (ctx) => persist(ctx.todos),
-    },
-  },
-  {
-    todo: "",
-    todos: (() => {
-      try {
-        return retrieve() || [];
-      } catch (e) {
-        return [];
-      }
-    })(),
-  }
-);
-
 function CheckInPage() {
-  const [state, send] = useMachine(persistedTodosMachine);
+  const [state, send] = useMachine(todosMachine);
 
-  const { todos, todo } = state.context;
+  const { todos, outstandingTodoInput, completedTodoInput } = state.context;
 
-  const handleInputKeyPress = (e) => {
+  const completedTodos = todos.filter((todo) => todo.completed);
+  const outstandingTodos = todos.filter((todo) => !todo.completed);
+
+  const handleOutstandingTodoChange = (e) => {
+    send("NEW.OUTSTANDING.TODO.CHANGE", { value: e.target.value });
+  };
+
+  const handleOutstandingTodoKeyPress = (e) => {
     if (e.key === "Enter") {
-      send("NEWTODO.COMMIT", { value: e.target.value });
+      send("NEW.OUTSTANDING.TODO.COMMIT", { value: e.target.value });
     }
   };
-  const handleInputChange = (e) => {
-    send("NEWTODO.CHANGE", { value: e.target.value });
+
+  const handleCompletedTodoChange = (e) =>
+    send("NEW.COMPLETED.TODO.CHANGE", { value: e.target.value });
+  const handleCompletedTodoKeyPress = (e) => {
+    if (e.key === "Enter")
+      send("NEW.COMPLETED.TODO.COMMIT", { value: e.target.value });
   };
+
   return (
     <div>
-      <input
-        placeholder="What needs to be done?"
-        autoFocus
-        onKeyPress={handleInputKeyPress}
-        onChange={handleInputChange}
-        value={todo}
-      />
-      {todos.map((todo) => {
-        return <Todo key={todo.id} todoRef={todo.ref} />;
-      })}
+      <div>
+        <p>Good morning, Anthony!</p>
+        <p>It's April 23, 2020. How are you feeling today?</p>
+      </div>
+
+      <div>
+        <span>Today's intentions:</span>
+        {outstandingTodos.map((todo) => {
+          return <Todo key={todo.id} todoRef={todo.ref} />;
+        })}
+        <input
+          type="text"
+          value={outstandingTodoInput}
+          onChange={handleOutstandingTodoChange}
+          onKeyPress={handleOutstandingTodoKeyPress}
+          placeholder="Tell us what you're going to do"
+        />
+        {state.matches("ready.outstandingTodoInput.error.empty") && (
+          <span className="text-red-700">This can't be empty!</span>
+        )}
+      </div>
+
+      <div>
+        <span>Yesterday's wins:</span>
+        {completedTodos.map((todo) => {
+          return <Todo key={todo.id} todoRef={todo.ref} />;
+        })}
+        <input
+          type="text"
+          value={completedTodoInput}
+          onChange={handleCompletedTodoChange}
+          onKeyPress={handleCompletedTodoKeyPress}
+          placeholder="Tell us what you did"
+        />
+        {state.matches("ready.completedTodoInput.error.empty") && (
+          <span className="text-red-700">This can't be empty!</span>
+        )}
+      </div>
     </div>
   );
 }
--- src/CheckInPage/todosMachine.js
+++ src/CheckInPage/todosMachine.js
@@ -1,6 +1,7 @@
 import { Machine, spawn, assign } from "xstate";
 import todoMachine from "./todoMachine";
 import { uuid } from "uuidv4";
+import { retrieve, persist } from "./storage";
 
 function createTodo(title) {
   return {
@@ -10,16 +11,101 @@ function createTodo(title) {
   };
 }
 
-const todosMachine = Machine({
-  id: "todos",
-  context: {
-    todo: "",
-    todos: [],
+const todosMachine = Machine(
+  {
+    id: "todos",
+    context: {
+      outstandingTodoInput: "",
+      completedTodoInput: "",
+      todos: [],
+    },
+    initial: "initializing",
+    states: {
+      initializing: {
+        entry: "getInitialTodos",
+        on: {
+          "": "ready",
+        },
+      },
+      ready: {
+        type: "parallel",
+        states: {
+          completedTodoInput: {
+            initial: "noError",
+            states: {
+              noError: {},
+              error: {
+                initial: "empty",
+                states: { empty: {} },
+              },
+            },
+          },
+          outstandingTodoInput: {
+            initial: "noError",
+            states: {
+              noError: {},
+              error: {
+                initial: "empty",
+                states: { empty: {} },
+              },
+            },
+          },
+        },
+      },
+      committingOutstandingTodo: {
+        entry: ["commitOutstandingTodo", "persist"],
+        on: {
+          "": "ready",
+        },
+      },
+      committingCompletedTodo: {
+        entry: ["commitCompletedTodo", "persist"],
+        on: {
+          "": "ready",
+        },
+      },
+    },
+    on: {
+      "NEW.OUTSTANDING.TODO.CHANGE": {
+        actions: "cacheOutstandingTodoChange",
+        target: "ready.outstandingTodoInput.noError",
+      },
+      "NEW.OUTSTANDING.TODO.COMMIT": [
+        {
+          cond: "isOutstandingTodoInputEmpty",
+          target: "ready.outstandingTodoInput.error.empty",
+        },
+        { target: "committingOutstandingTodo" },
+      ],
+      "NEW.COMPLETED.TODO.CHANGE": {
+        actions: "cacheCompletedTodoChange",
+        target: "ready.completedTodoInput.noError",
+      },
+      "NEW.COMPLETED.TODO.COMMIT": [
+        {
+          cond: "isCompletedTodoInputEmpty",
+          target: "ready.completedTodoInput.error.empty",
+        },
+        { target: "committingCompletedTodo" },
+      ],
+      "TODO.COMMIT": {
+        actions: ["commitTodoChange", "persist"],
+      },
+      "TODO.DELETE": {
+        actions: ["deleteTodo", "persist"],
+      },
+    },
   },
-  initial: "initializing",
-  states: {
-    initializing: {
-      entry: assign({
+  {
+    guards: {
+      isOutstandingTodoInputEmpty: (context) =>
+        context.outstandingTodoInput.length === 0,
+      isCompletedTodoInputEmpty: (context) =>
+        context.completedTodoInput.length === 0,
+    },
+    actions: {
+      persist: (ctx) => persist(ctx.todos),
+      getInitialTodos: assign({
         todos: (context) => {
           return context.todos.map((todo) => ({
             ...todo,
@@ -27,60 +113,58 @@ const todosMachine = Machine({
           }));
         },
       }),
-      on: {
-        "": "all",
-      },
-    },
-    all: {},
-  },
-  on: {
-    "NEWTODO.CHANGE": {
-      actions: assign({
-        todo: (_, event) => event.value,
+      commitTodoChange: assign({
+        todos: (ctx, event) => {
+          return ctx.todos.map((todo) => {
+            return todo.id === event.todo.id
+              ? { ...todo, ...event.todo, ref: todo.ref }
+              : todo;
+          });
+        },
+      }),
+      deleteTodo: assign({
+        todos: (ctx, e) => ctx.todos.filter((todo) => todo.id !== e.id),
+      }),
+      cacheOutstandingTodoChange: assign({
+        outstandingTodoInput: (_, event) => event.value,
+      }),
+      cacheCompletedTodoChange: assign({
+        completedTodoInput: (_, event) => event.value,
+      }),
+      commitOutstandingTodo: assign({
+        outstandingTodoInput: "",
+        todos: (context, event) => {
+          const newTodo = createTodo(event.value);
+          return context.todos.concat({
+            ...newTodo,
+            ref: spawn(todoMachine.withContext(newTodo)),
+          });
+        },
+      }),
+      commitCompletedTodo: assign({
+        completedTodoInput: "",
+        todos: (context, event) => {
+          const newTodo = createTodo(event.value);
+          newTodo.completed = true;
+          return context.todos.concat({
+            ...newTodo,
+            ref: spawn(todoMachine.withContext(newTodo)),
+          });
+        },
       }),
     },
-
-    "NEWTODO.COMMIT": {
-      cond: (_, event) => event.value.trim().length > 0,
-      actions: [
-        assign({
-          todo: "",
-          todos: (ctx, event) => {
-            const newTodo = createTodo(event.value.trim());
-            return ctx.todos.concat({
-              ...newTodo,
-              ref: spawn(todoMachine.withContext(newTodo)),
-            });
-          },
-        }),
-        "persist",
-      ],
-    },
-
-    "TODO.COMMIT": {
-      actions: [
-        assign({
-          todos: (ctx, event) => {
-            return ctx.todos.map((todo) => {
-              return todo.id === event.todo.id
-                ? { ...todo, ...event.todo, ref: todo.ref }
-                : todo;
-            });
-          },
-        }),
-        "persist",
-      ],
-    },
-
-    "TODO.DELETE": {
-      actions: [
-        assign({
-          todos: (ctx, e) => ctx.todos.filter((todo) => todo.id !== e.id),
-        }),
-        "persist",
-      ],
-    },
   },
-});
+  {
+    outstandingTodoInput: "",
+    completedTodoInput: "",
+    todos: (() => {
+      try {
+        return retrieve() || [];
+      } catch (e) {
+        return [];
+      }
+    })(),
+  }
+);
 
 export default todosMachine;
